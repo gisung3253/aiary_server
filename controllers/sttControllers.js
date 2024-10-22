c3f24f46da27d7257007a38485cb27388397c52e
gisung3253 <gisung3253@naver.com>
2024-10-22 14:19:16 +0900

    asd

--- controllers/sttControllers.js
+++ controllers/sttControllers.js
@@ -1,6 +1,18 @@
 import multer from 'multer';
 import speech from '@google-cloud/speech';
-import credentials from './key.json' assert { type: "json" };
+
+const credentials = {
+  type: process.env.GOOGLE_TYPE,
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),  // 줄바꿈 처리
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+};
 
 
 const upload = multer();
